0c733c575f8f6bc135e92bec2a5bd545c26d3bda
939565502 <60669943+939565502@users.noreply.github.com>
2020-02-11 15:36:42 -0500

    Coin
    add Prefeb, Coin Matoeral. Score UI

---
+++ inferno/Assets/Player.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Player : MonoBehaviour
+{
+    
+    public Text text;
+    private int score=0;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ////void OnCollisionEnter(Collision collision)
+        //{
+        //    //string name = collision.collider.name;
+        //        if(collision.collider.tag == "PickUp"){
+        //        Destroy(collision.collider.gameObject);
+        //    }
+
+    
+
+        }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.tag == "Pick Up")
+        {
+            score++;
+            text.text = score.ToString();
+            Destroy(collider.gameObject);
+
+
+        }
+    }
+
+    
+
+}
+
